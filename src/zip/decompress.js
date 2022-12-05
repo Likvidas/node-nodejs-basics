a232fb03045e71c3162ad26da5700cf25c1d3235
Likvidas <bobento@yandex.ru>
2022-12-06 01:11:49 +0300

    feat: added a solution 'zip/decompress' task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { createGunzip } from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const archivePath = join(__dirname, 'files/archive.gz');
+    const targetPath = join(__dirname, 'files/fileToCompress.txt');
+
+    const gunzip = createGunzip();
+    const source = createReadStream(archivePath);
+    const destination = createWriteStream(targetPath);
+
+    source.pipe(gunzip).pipe(destination);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await decompress();
