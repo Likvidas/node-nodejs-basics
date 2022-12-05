3a15e9c7f28b5ddd5a3db62343f27f152a9c7dfc
Likvidas <bobento@yandex.ru>
2022-12-06 00:45:42 +0300

    feat: added a solution 'sreams/transform' task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  const resultData = new Transform({
+    transform(chank, encoding, callback) {
+      callback(null, chank.toString().split('').reverse().join(''));
+      process.exit(1);
+    },
+  });
+
+  stdin(resultData).pipe(stdout);
 };
 
 await transform();
