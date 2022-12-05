9bb4d42b1190fb2d367979dd84dd155a6774a57c
Likvidas <bobento@yandex.ru>
2022-12-05 23:17:56 +0300

    feat: added a solution 'cli/args' task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+import { argv } from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = argv.slice(2);
+  let result = '';
+
+  args.forEach((argument) => {
+    if (argument.includes('--')) {
+      result += argument.replace('--', '');
+    } else {
+      result += ` is ${argument}, `;
+    }
+  });
+  console.log(result);
 };
 
 parseArgs();
